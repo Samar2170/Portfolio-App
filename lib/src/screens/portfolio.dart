a18e092f7fe412b0999d18dc5fcac4d1af8603bc
samar2170 <scipioresearch2020@gmail.com>
2023-07-06 15:54:15 +0530

    portfolio tab view

--- lib/src/screens/portfolio.dart
+++ lib/src/screens/portfolio.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio_app/src/widgets/portfolio_nav_list.dart';
-
-import '../data.dart';
+import '../routing.dart';
 
 
 class PortfolioScreen extends StatefulWidget {
@@ -14,16 +12,39 @@ class PortfolioScreen extends StatefulWidget {
 class _PortfolioScreenState extends State<PortfolioScreen>
     with SingleTickerProviderStateMixin {
   
-  // late TabController _tabController;
+  late TabController _tabController;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _tabController = TabController(
-  //     length: 3,
-  //     vsync: this
-  //   )..addListener(_handleTabIndexChanged);
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this
+    )..addListener(_handleTabIndexChanged);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final newPath = _routeState.route.pathTemplate;
+    if (newPath.startsWith('/portfolio/holdings/')) {
+      _tabController.index = 0;
+    } else if (newPath.startsWith('/portfolio/snapshot/')) {
+      _tabController.index = 2;
+    } else {
+      _tabController.index = 1;
+    }
+  }
+
+
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabIndexChanged);
+    super.dispose();
+  }
+
 
 
   @override
@@ -31,10 +52,44 @@ class _PortfolioScreenState extends State<PortfolioScreen>
     return Scaffold(
       appBar: AppBar(
         title: const Text('Portfolio'),
-      ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Holdings'),
+            Tab(text: 'Agg Holdings'),
+            Tab(text: 'Snapshot'),
+          ],
+        )
+        ),
       body: 
-          PortfolioNavList(navOptions: navOptions.where((navOption) => navOption.navCategory.category == 'portfolio').toList()),
+          TabBarView(
+            controller: _tabController,
+            children:  const [
+              Text('Holdings'),
+              Text('Agg Holdings'),
+              Text('Snapshot')
+            ]
+          ),
       );
+  }
+
+  RouteState get _routeState => RouteStateScope.of(context);
+
+  void _handleTabIndexChanged() {
+    switch (_tabController.index) {
+      case 1:
+        _routeState.go('portfolio/agg_holdings/');
+        break;
+      case 2:
+        _routeState.go('portfolio/snapshot/');
+        break;
+      case 0:
+        _routeState.go('portfolio/holdings/');
+        break;
+      default:
+        _routeState.go('portfolio/agg_holdings/');
+        break;
+    }
   }
 }
 
